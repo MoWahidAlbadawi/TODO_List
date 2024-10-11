04367d8a74349ad94fd6f0cc3de416337d987425
MoWahidAlbadawi <mohammadwahidalbadawi@gmail.com>
2024-10-12 01:00:26 +0300

    comitting 12

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,66 @@ import { useState , useEffect} from "react";
 import TaskInput from "./components/TaskInput"
 import TaskItem from "./components/TaskItem";
 import Status from "./components/Status";
+import { v4 as uuidv4 } from "uuid";
 function App() {
   const [tasks , setTasks] = useState([]);
-    function RecieveNewTask (task) {
-      setTasks((prevTasks) => [...prevTasks , task]);
-    }
-    function deleteItem (myTask) {
-      setTasks((tasks) => tasks.filter((task) => task!==myTask));
-    }
+  const [newTask , setNewTask] = useState(null);
+  const [userId , setUserId] = useState(null);
+  const [error , setError] = useState(null);
+  function RecieveNewTask (task) {
+    setNewTask(task);
+  }
     let messageTasks = '';
+    useEffect(() => {
+      let storedUserId = localStorage.getItem('userId');
+      if(!storedUserId) {
+        storedUserId = uuidv4();
+        localStorage.setItem('userId',storedUserId);
+      }
+      setUserId(storedUserId);
+    } ,[]);  
+    useEffect(() => {
+      async function sendingData() {
+        try {
+        const response = await fetch(`https://foodapp-5ab37-default-rtdb.firebaseio.com//tasks/${userId}.json`,{
+          method : 'POST',
+          body : JSON.stringify(newTask),
+        });
+        if(!response.ok) 
+          throw new Error('something wrong please try again!');
+      }
+      catch {
+        console.log('failed send task!');
+      }
+    }
+      sendingData();
+    },[newTask]);
+    useEffect(() => {
+      async function gettingData() {
+        try {
+        const response = await fetch(`https://foodapp-5ab37-default-rtdb.firebaseio.com//tasks/${userId}.json`);
+        if(!response.ok) {
+          throw new Error('something wrong please try again!');
+      }
+      const data = await response.json();
+      const tasksFromDb = [];
+      for (const key in data) {
+        tasksFromDb.push({
+          id : key,
+          task : data[key],
+        }); 
+      }
+      setTasks(tasksFromDb);
+    }
+    catch(error) {
+      setError(error.message);
+    }
+    }
+    gettingData();
+    },[tasks]);
     if(tasks.length===0)
       messageTasks = <p className="text-gray-400 text-semibold text-center ml-4">you are done!</p>
-    return (
+      return (
     <>
     <div className="flex justify-center mt-24">
       <div className="bg-cyan-900 rounded-lg  py-6 px-2">
@@ -21,9 +69,10 @@ function App() {
       <TaskInput onAddTask = {RecieveNewTask}/>
       <span className="font-extrabold text-gray-400 text-left ml-4">To Do</span>
       {messageTasks}
-      <ul>
+      {error && <p>{error}</p>}
+              <ul>
         {tasks.map((task) => (
-          <TaskItem task = {task} onDeleteItem= {deleteItem}/>
+          <TaskItem task = {task.task} y={task.id} userId={userId}/>
         ))}
       </ul>
       </div>
--- src/components/TaskItem.jsx
+++ src/components/TaskItem.jsx
@@ -1,11 +1,18 @@
 const TaskItem = (props) => {
-    function deleteItem () {
-        props.onDeleteItem(props.task);
+    async function deleteItem () {
+        try {
+        const response = await fetch(`https://foodapp-5ab37-default-rtdb.firebaseio.com/tasks/${props.userId}/${props.y}.json` ,{
+            method : 'DELETE',
+        });
+    }
+    catch {
+        console.log('del failed');
+    }
     }  
     return  <li className="text-slate-200 mx-4 mt-2 bg-slate-500 rounded pl-4 pr-3 py-2 flex justify-between">
         <p>{props.task}</p>
         <img width="32" height="32" src="https://img.icons8.com/fluency/48/filled-trash.png" alt="filled-trash"
-        onClick={deleteItem} className="cursor-pointer"/>
+         className="cursor-pointer" onClick={deleteItem}/>
     </li>
 }
 export default TaskItem;
